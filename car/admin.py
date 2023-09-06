37de995e18a31e3af42e4f905a98d858ca5ec7cf
AutocraticKhan <Autocratic.jalal@gmail.com>
2023-09-06 14:13:46 +0500

    catogory added

--- car/admin.py
+++ car/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import  Car
+from .models import  Vehicle, Group
 # Register your models here.
 
 
-@admin.register(Car)
+@admin.register(Vehicle)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price','available', 'bags', 'passengers']
     list_filter = ['available', 'transmission', 'group', 'passengers']
     list_editable = ['price', 'available']
     # prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Group)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['group', 'slug']
+    list_filter = ['group']
+ 
--- car/models.py
+++ car/models.py
@@ -1,48 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    group = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,blank=True ,unique=True)
 
-class Car(models.Model):
+    class Meta:
+        ordering = ['group']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
-    class Group(models.TextChoices):
-        CAR = 'CR', 'Car/Sedan'
-        VAN = 'VN', 'Van'
-        SUV = 'SV', 'SUV'
-        SPECIAL = 'SP', 'Special'
-        SVAGON = 'Sw', 'Station Wagon'
+    def __str__(self):
+        return self.group
 
-
+class Vehicle(models.Model):
     class Transmission(models.TextChoices):
         AUTOMATIC = 'A', 'Automatic'
         MANUAL = 'M', 'Manual'
 
-
-
-
-    group = models.CharField(max_length=2,
-                            choices=Group.choices,
-                            default=Group.CAR)
-
-    transmission = models.CharField(max_length=1,
-                        choices=Transmission.choices,
-                        default=Transmission.MANUAL)
-
+    group = models.ForeignKey(Group, related_name='car_type', on_delete=models.CASCADE)
+    transmission = models.CharField(max_length=1, choices=Transmission.choices, default=Transmission.MANUAL)
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,unique=True)
-    image = models.ImageField(upload_to='vehicles/%Y/%m/%d',blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='vehicles/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=5,decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     available = models.BooleanField(default=True)
     bags = models.IntegerField()
     passengers = models.IntegerField()
 
-
-
     class Meta:
         ordering = ['name']
         indexes = [
-        models.Index(fields=['id', 'slug']),
-        models.Index(fields=['name']),
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
         ]
+
     def __str__(self):
         return self.name

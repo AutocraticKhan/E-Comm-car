7363a3f7f6f8908817a9d340440b72b3007948e1
AutocraticKhan <Autocratic.jalal@gmail.com>
2023-09-12 12:12:00 +0500

    datetime added saperatel

--- car/admin.py
+++ car/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 
-from .models import Category, Sub_category, Vehicle
+from .models import Category, Sub_category, Vehicle, Return_datetime,Return_location,Pickup_datetime,Pickup_location
 
+
+@admin.register(Pickup_location)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pickup_location')
+
+@admin.register(Return_location)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'return_location')
+
+@admin.register(Pickup_datetime)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pickup_datetime')
+
+@admin.register(Return_datetime)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'return_datetime')
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):

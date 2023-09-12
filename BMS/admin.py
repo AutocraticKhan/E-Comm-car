6b6f36d388c2c6626a329cc45d51168718c1a318
AutocraticKhan <Autocratic.jalal@gmail.com>
2023-09-12 13:11:45 +0500

    BMS added

--- BMS/admin.py
+++ BMS/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import BookingManagement
 # Register your models here.
+
+@admin.register(BookingManagement)
+class BMSAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'VAT_percentage',
+        'pay_on_arrival_discount_percentage',
+        'pay_online_discount_percentage',
+        'super_collision_damage_waiver_price',
+        'twu_price',
+        'baby_seat_price',
+        'booster_seat_price',
+        'roof_rack_price',
+    )
--- BMS/models.py
+++ BMS/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class BookingManagement(models.Model):
+    VAT_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=19.00)
+    pay_on_arrival_discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=10.00)
+    pay_online_discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=15.00)
+    super_collision_damage_waiver_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    twu_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    baby_seat_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    booster_seat_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    roof_rack_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return "Booking Management Configuration"
